cd0aff96997bbb3a8be90af2fdf9d28d4260fed9
Brendan Goodenough <brendan@goodenough.nz>
2020-08-19 11:04:44 +0200

    Add basic example of querying catalogue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+const CATALOGUE = gql`
+  query {
+    catalogue(language: "en", path: "/") {
+      children {
+        ...item
+        ...product
+
+        children {
+          ...item
+          ...product
+        }
+      }
+    }
+  }
+
+  fragment item on Item {
+    id
+    name
+    type
+    path
+  }
+
+  fragment product on Product {
+    id
+    language
+    vatType {
+      name
+      percent
+    }
+    isVirtual
+    isSubscriptionOnly
+    variants {
+      id
+      name
+      sku
+      price
+      stock
+      isDefault
+    }
+  }
+`
 
 function App() {
+  const { loading, error, data } = useQuery(CATALOGUE)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.catalogue.children.map((item) => {
+          if (item.type === 'folder' && item.children) {
+            return (
+              <li>
+                {item.name}
+                <ul>
+                  {item.children.map((child) => (
+                    <li>{child.name}</li>
+                  ))}
+                </ul>
+              </li>
+            )
+          }
+
+          return <li>{item.name}</li>
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
